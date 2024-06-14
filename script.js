edeeacd89bb31196a01816b634a0590d8c6de9e5
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-06-14 18:48:27 +0530

    check

--- script.js
+++ script.js
@@ -105,6 +105,11 @@ function startFallingEmojis() {
    }
 }
 
+document.addEventListener('mousemove', function(e) {
+   const cursor = document.querySelector('.cursor');
+   cursor.style.left = e.pageX + 'px';
+   cursor.style.top = e.pageY + 'px';
+});
 
 
 
